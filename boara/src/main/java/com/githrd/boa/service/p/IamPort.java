64091375e2ff16e535d1da6ba2ca12104bdf600f
aknod <aknod@DESKTOP-3HQJE7P>
2022-09-13 22:55:51 +0900

    2022/09/13 박소연 주석 추가

--- boara/src/main/java/com/githrd/boa/service/p/IamPort.java
+++ boara/src/main/java/com/githrd/boa/service/p/IamPort.java
@@ -55,47 +55,46 @@ public class IamPort {
    }
    
    //토큰 발급
-   public String getToken() throws IOException {
+   public String getToken() throws IOException { //버퍼를 사용하므로 예외 처리
        
       
+	  //REST api 호출 위해 사용 (웹을 통해 데이터 주고 받을 때 사용)
       HttpsURLConnection conn = null;
 
       //절대 경로로 사이트와 연결
       URL url = new URL("https://api.iamport.kr/users/getToken");
-
-      //받아온 페이지 정보를 형변환 해준다.
       conn = (HttpsURLConnection) url.openConnection();
       
-      //전달 방식, 수송식 방식 설정
+      //전달 방식은 post, json 형식으로 데이터 주고 받는다.
       conn.setRequestMethod("POST");
       conn.setRequestProperty("Content-type", "application/json");
       conn.setRequestProperty("Accept", "application/json");
       
-      //스트림 사용 위해서 true 설정
+      //서버와 통신하고자 true 설정
       conn.setDoOutput(true);
       
       //json 객체
       JsonObject json = new JsonObject();
 
-      //관리자 정보 보내주고
+      //json 형식으로 관리자 정보 보내주고
       json.addProperty("imp_key", "1050448697343442");
       json.addProperty("imp_secret", "7c180050ae3ff7479c4d2337eb77e46d85d5cc1b399ba4359b4beaeecef0947019f695cec9bf99e9");
       
-      //버퍼 사용
+      //할당된 버퍼에 값 넣어주기
       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
       
-      bw.write(json.toString());
-      bw.flush();
-      bw.close();
+      bw.write(json.toString()); // 버퍼에 있는 값 모두 출력 (문자열 형식으로)
+      bw.flush(); //남은 데이터 모두 출력
+      bw.close(); //스트림 닫기
       
-      //utf-8 인코딩 방식으로 읽어온다.
+      //utf-8 인코딩 방식으로 토큰을 읽어온다.
       BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
-
-      //json을 자바 객체로 바꿔준다.
+            
+      //gson을 사용해 json을 hashMap으로 변환
       Gson gson = new Gson();
       String response = gson.fromJson(br.readLine(), Map.class).get("response").toString();
       String token = gson.fromJson(response, Map.class).get("access_token").toString();
-
+      
       //버퍼 닫아주고
       br.close();
       
@@ -110,31 +109,30 @@ public class IamPort {
    //결제 취소   
    public boolean payMentCancle(String access_token, String imp_uid, int gnp) throws IOException  {
       
+	  //REST api 호출 위해 사용 (웹을 통해 데이터 주고 받을 때 사용)
       HttpsURLConnection conn = null;
       
       //절대 경로로 사이트와 연결
       URL url = new URL("https://api.iamport.kr/payments/cancel");
- 
-      //받아온 페이지 정보를 형변환 해준다.
       conn = (HttpsURLConnection) url.openConnection();
  
-      //전달 방식, 수송식, 인증 방식 설정 (토큰으로)
+      //전달 방식은 post, json 형식으로 데이터 주고 받는다.
       conn.setRequestMethod("POST");
       conn.setRequestProperty("Content-type", "application/json");
       conn.setRequestProperty("Accept", "application/json");
       conn.setRequestProperty("Authorization", access_token);
  
-      //스트림 사용 위해서 true 설정
+      //서버와 통신하고자 true 설정
       conn.setDoOutput(true);
       
       //json 객체
       JsonObject json = new JsonObject();
       
-      //결제번호, 금액 생성
+      //json 형식으로 결제번호, 금액 생성
       json.addProperty("imp_uid", imp_uid);
       json.addProperty("amount", gnp);
  
-      //버퍼로 데이터 입력
+      //할당된 버퍼에 데이터 입력
       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
  
       bw.write(json.toString());
